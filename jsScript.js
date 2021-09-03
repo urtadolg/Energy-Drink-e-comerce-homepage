fdf966ead494c8cff27aaa07a701ca631673fa1b
Leandro <leandro.urtado25@gmail.com>
2021-09-03 15:07:40 -0300

    Added highlight on bestsellers and category items when mouse hovering.

--- jsScript.js
+++ jsScript.js
@@ -1,5 +1,18 @@
 const menu = document.querySelector(".btnMenu");
+const bestSellerItem = document.querySelectorAll('.itemBs');
+const btnComprar = document.querySelectorAll('.btnBs');
 const menuPanel = document.querySelector(".btnMenuPanel");
+
+
 menu.addEventListener("click", () => {
   menuPanel.classList.toggle("turnOn");
 });
+
+for(let i=0;i<bestSellerItem.length;i++){
+bestSellerItem[i].addEventListener('mouseover',(event)=>{
+  btnComprar[i].style.backgroundColor = 'green';
+})
+bestSellerItem[i].addEventListener('mouseleave',(event)=>{
+  btnComprar[i].style.backgroundColor = '';
+})
+}
